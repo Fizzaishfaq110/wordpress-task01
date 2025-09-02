507be87553ca804c6a4771dbdb0eedda53fb9fae
fizzaishfaq110 <fizzaishfaq313@gmail.com>
2025-09-02 09:57:09 +0300

    add: custom plugins, admin notice

---
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: My First Plugin
+Description: A simple plugin to learn WordPress development.
+Version: 1.0
+Author: Your Name
+*/
+function my_first_plugin_footer_message()
+{
+    echo '<p style="text-align: center;">Hello from My First Plugin!</p>';
+}
+add_action('wp_footer', 'my_first_plugin_footer_message');
+
+function my_first_shortcode()
+{
+    return '<p style="color: purple;"><p>This is my first shortcode!</p>';
+}
+add_shortcode('my_hello', 'my_first_shortcode');
+
+function my_first_plugin_admin_notice()
+{echo '
+Plugin Learning Rocks!
+';}add_action('admin_notices', 'my_first_plugin_admin_notice');
